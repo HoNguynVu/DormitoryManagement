a984a7b0c47788157429092c649aa30395733305
Hồ Nguyên Vũ <23521805@gm.uit.edu.vn>
2025-12-11 21:17:39 +0700

    change to migration ver2

--- BusinessObject/Base/Person.cs
+++ BusinessObject/Base/Person.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BusinessObject.Entities
+namespace BusinessObject.Base
 {
     // Abstract để EF Core không tạo bảng Persons
     public abstract class Person
