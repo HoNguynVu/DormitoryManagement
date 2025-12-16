b428b608130f22fa785e942f9a0a2dc67a91a01a
Nguyen Van Pham <23521136@gm.uit.edu.vn>
2025-12-16 23:14:03 +0700

    Add Confirm Payment Success Dto

---
+++ BusinessObject/DTOs/ConfirmDTOs/HealthInsurancePurchaseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTOs.ConfirmDTOs
+{
+    public class HealthInsurancePurchaseDto : BaseConfirmDto
+    {
+        public string InsurancePeriod { get; set; } = string.Empty; // Giai đoạn bảo hiểm (VD: 2024 - 2025)
+        public DateOnly CoverageStartDate { get; set; } // Ngày hiệu lực
+        public DateOnly CoverageEndDate { get; set; }  // Ngày hết hạn
+        public decimal Cost { get; set; }            // Số tiền đã đóng
+        public string Status { get; set; } = "Paid"; // Trạng thái
+    }
+}
