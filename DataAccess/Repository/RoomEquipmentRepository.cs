5eb18bce74c60b31d78b88b5d4680fc88c293868
Nguyen Van Pham <23521136@gm.uit.edu.vn>
2025-12-22 22:31:21 +0700

    add roomequipment repo

---
+++ DataAccess/Repository/RoomEquipmentRepository.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObject.Entities;
+using DataAccess.Interfaces;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class RoomEquipmentRepository : GenericRepository<RoomEquipment>, IRoomEquipmentRepository
+    {
+        public RoomEquipmentRepository(DormitoryDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<RoomEquipment>> GetEquipmentsByRoomIdAsync(string roomId)
+        {
+            return await _dbSet
+                .Include(re => re.Equipment)
+                .Include(re => re.Room)
+                .Where(re => re.RoomID == roomId)
+                .ToListAsync();
+        }
+
+    }
+}
