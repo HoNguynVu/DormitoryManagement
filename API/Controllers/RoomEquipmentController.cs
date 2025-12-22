4f9ae7a8d87576a9a48999feedc10244d375ef70
Nguyen Van Pham <23521136@gm.uit.edu.vn>
2025-12-22 23:59:25 +0700

    add roomequipment controller

---
+++ API/Controllers/RoomEquipmentController.cs
@@ -0,0 +1,37 @@
+﻿using API.Services.Interfaces;
+using BusinessObject.DTOs.EquipmentDTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomEquipmentController : ControllerBase
+    {
+        private readonly IRoomEquipmentService _roomEquipmentService;
+        public RoomEquipmentController(IRoomEquipmentService roomEquipmentService)
+        {
+            _roomEquipmentService = roomEquipmentService;
+        }
+
+        [HttpPost("change-status")]
+        public async Task<IActionResult> ChangeStatus([FromBody] ChangeStatusRoomEquipmentDto dto)
+        {
+            var result = await _roomEquipmentService.ChangeStatusAsync(
+                dto.RoomId,
+                dto.EquipmentId,
+                dto.Quantity,
+                dto.FromStatus,
+                dto.ToStatus
+            );
+            if (result.Success)
+            {
+                return Ok(new { message = result.Message });
+            }
+            else
+            {
+                return StatusCode(result.StatusCode, new { error = result.Message });
+            }
+        }
+    }
+}
---
+++ BusinessObject/DTOs/EquipmentDTOs/ChangeStatusRoomEquipmentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTOs.EquipmentDTOs
+{
+    public class ChangeStatusRoomEquipmentDto
+    {
+        public string RoomId { get; set; }  = string.Empty;
+        public string EquipmentId { get; set; } = string.Empty;
+        public int Quantity { get; set; } 
+        public string FromStatus { get; set; } = string.Empty;
+        public string ToStatus { get; set; } = string.Empty;
+    }
+}
